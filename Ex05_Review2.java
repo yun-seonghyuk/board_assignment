6f0ff21d196bf507ae45d89ff31e65b7e8488ddf
yun <dbstjdgur78@naver.com>
2022-10-16 23:47:15 +0900

    두번째 커밋
    version_success

--- Ex05_Review2.java
+++ Ex05_Review2.java
@@ -16,13 +16,11 @@ public class Ex05_Review2 {
 	public void loginMenu(String log) {
 		while(true) {
 			System.out.println(log+"님 환영합니다.");
-//			board.add(new Board("첫번째 게시글","고길동","왔다 갑니다! 즐거운 하루 되세요!"));
-//			board.add(new Board("안녕하세요!","이길동","대출은 OOO-OOOO!!"));
-//			board.add(new Board("털어갑니다.","홍길동","베풀면서 삽시다!!!"));
 			System.out.println("1.글목록보기 2.글작성 3.로그아웃");
 			int select = scan.nextInt();
 			if(select == 1) {
-				
+				boardList();
+				viewPost();
 			}else if(select == 2) {
 				writing();
 			}else if(select == 3) {
@@ -35,6 +33,78 @@ public class Ex05_Review2 {
 		}
 	}
 
+	private void viewPost() {
+		System.out.println("--------------------------------------");
+		// 3) 글 상세보기는 글번호를 입력하면 제목, 작성자, 글 내용을 출력
+		System.out.println("상세보기");
+		System.out.print("보고싶은 글 번호를 입력하세요>>");
+		int boardNum = scan.nextInt()-1;
+		
+		if(0<=boardNum && boardNum<board.size()) {
+			String title = board.get(boardNum).getTitle();
+			String writer = board.get(boardNum).getWiter();
+			String content = board.get(boardNum).getContent();
+			
+			System.out.printf("%s \t %s \n",title,writer);
+			System.out.println(content);
+			updateAndDelete(boardNum);
+		}else {
+			System.out.println("해당 번호의 게시글이 없습니다.");
+		}
+		
+	}
+	private void updateAndDelete(int boardNum) {
+			System.out.println("1.글수정 2. 글삭제 3.뒤로가기");
+			System.out.print(">>");
+			int selectNum = scan.nextInt();
+			
+			switch(selectNum) {
+			case 1:
+				System.out.println("글을 수정합니다.");
+				System.out.println("글 비밀번호를 입력하세요!");
+				System.out.print(">>");
+				String password = scan.nextLine();
+				
+				if(Ex05_Review.list.get(boardNum).getPw().equals(password)) {
+					System.out.print("제목를 입력하세요 >>");
+					String title = scan.nextLine();
+					System.out.print("글내용를 입력하세요 >>");
+					String content = scan.nextLine();
+					
+					board.get(boardNum).setTitle(title);
+					board.get(boardNum).setContent(content);
+					
+					System.out.println("수정이 완료되었습니다.");
+				}else {
+					System.out.println("비밀번호가 틀렸습니다.");
+				}
+				
+				break;
+			case 2:
+				System.out.println("글을 삭제합니다.");
+				System.out.println("글 비밀번호를 입력하세요!");
+				System.out.print(">>");
+				password = scan.next();
+				
+				if(Ex05_Review.list.get(boardNum).getPw().equals(password)) {
+					board.remove(boardNum);
+					System.out.println("삭제가 완료되었습니다.");
+				}else {
+					System.out.println("비밀번호가 틀렸습니다.");
+				}
+				
+				break;
+			case 3:
+				System.out.println("뒤로가기");
+				break;
+			}
+			
+			
+			// 6) 위 기능이 정상적으로 작동했는지 전체 글 내용을 출력해 봅시다.
+			//boardList();
+			
+			}
+		
 	private void writing() {
 		System.out.println("글 작성");
 		System.out.println("작성자를 입력하세요 >>");
--- Member.java
+++ Member.java
@@ -4,7 +4,7 @@ public class Member {
 	private String id;
 	private String pw;
 	private String name;
-	
+
 	public Member(String id, String pw, String name) {
 		this.id = id;
 		this.pw = pw;
